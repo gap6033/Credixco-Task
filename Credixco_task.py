84d4a92392d80a4e5c970f2c0aaa00405e93ff6a
Gaurang Pateriya <51721897+gap6033@users.noreply.github.com>
2021-03-11 04:33:44 +0530

    Update Credixco_task.py

--- Credixco_task.py
+++ Credixco_task.py
@@ -17,74 +17,72 @@ proxy_list = []
 # looping through the cols list
 for col in cols:
     try:
-        # if the address belongs to USA its added
-        if col[3]=='United States':
+        # address appended if from USA and supports https 
+        if col[3]=='United States' and col[6] == 'yes':
             # appending only the ip address and the port adress
-            proxy_list.append(col[0]+':'+col[1])
+            proxy_list.append('https://'+col[0]+':'+col[1])
     except:
         # this is to skip the error raised due to first empty nested list of cols
         pass
 
-# function to access the site using proxy rotation
+# function to access the site using rotation proxy
 def fetch(url):
     proxy_index = 0
     while proxy_index < len(proxy_list):
         try:
             # testing if current proxy works
             print("Current Proxy", proxy_list[proxy_index])
-            res = requests.get(url, proxy_list={'http':proxy_list[proxy_index], 'https':proxy_list[proxy_index]}, timeout=5)
+            res = requests.get(url, proxies={'https':proxy_list[proxy_index]}, timeout=5)
             return res
         except:
             # Moving to next proxy if current proxy doesn't work
             print("Current Proxy Failed....")
             proxy_index += 1
-        # In case none of the proxies work
-        if proxy_index == len(proxy_list):
-            print("None of the proxies work")
-            user_response = input("Would you like to access site from your own ip address? Y|N: ")
-            if user_response =="Y" or "y":
-                res = requests.get(url)
-                return res
-            else:
-                print("You need working proxy ip address")
-                quit()
-
-
-# accessing website to be parsed
-res = fetch("https://www.midsouthshooterssupply.com/dept/reloading/primers?currentpage=1")
-soup = BeautifulSoup(res.text, 'html.parser')
-
-# accessing the prices of all the products
-price_list = soup.find_all("span", {"class":"price"})
-# accessing the names of all the products
-name_list = soup.find_all("a", {"class":"catalog-item-name"})
-# accessing the availability of all the products
-availability = soup.find_all("span", {"class":"out-of-stock"})
-# accessing the manufacturers of all the products
-manufacturers = soup.find_all("a", {"class":"catalog-item-brand"})
-total_products = len(price_list)
+    
 
 # Creating a list of dictionary
 # creating empty list
 products = []
-i = 0
-while i < total_products:
-    # creating empty dictionary
-    product_info = {}
-    # setting price key store price float value
-    product_info['price'] = float(price_list[i].text.lstrip("$"))
-    # setting title key store title value
-    product_info['title'] = name_list[i].text
-    # storing the availability data into dictionary
-    if availability[i].text == 'Out of Stock':
-        product_info['stock'] = False
-    else:
-        product_info['stock'] = True
-    # setting manufacturer's key store manufacturers name as string value
-    product_info['maftr'] = manufacturers[i].text
-    # appending the dictionary to products list
-    products.append(product_info)
-    i = i+ 1
+page = 1
+# We have 2 pages to parse
+while page < 3:
+    # accessing website to be parsed
+    res = fetch(f"https://www.midsouthshooterssupply.com/dept/reloading/primers?currentpage={page}")
+    # in case proxies do not work
+    if res == None:
+        print(f"None of the proxies work scraping page {page} data from own IP address")
+        res = requests.get(f"https://www.midsouthshooterssupply.com/dept/reloading/primers?currentpage={page}")
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    # accessing the prices of all the products
+    price_list = soup.find_all("span", {"class":"price"})
+    # accessing the names of all the products
+    name_list = soup.find_all("a", {"class":"catalog-item-name"})
+    # accessing the availability of all the products
+    availability = soup.find_all("span", {"class":"out-of-stock"})
+    # accessing the manufacturers of all the products
+    manufacturers = soup.find_all("a", {"class":"catalog-item-brand"})
+    total_products = len(price_list)
+
+    i = 0
+    while i < total_products:
+        # creating empty dictionary
+        product_info = {}
+        # setting price key store price float value
+        product_info['price'] = float(price_list[i].text.lstrip("$"))
+        # setting title key store title value
+        product_info['title'] = name_list[i].text
+        # storing the availability data into dictionary
+        if availability[i].text == 'Out of Stock':
+            product_info['stock'] = False
+        else:
+            product_info['stock'] = True
+        # setting manufacturer's key store manufacturers name as string value
+        product_info['maftr'] = manufacturers[i].text
+        # appending the dictionary to products list
+        products.append(product_info)
+        i = i+1
+    page = page+1
 
 # converting list of dictionary into json
 json_object = json.dumps(products, indent = 4)
